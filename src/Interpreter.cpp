85d6bc194e7985ae8442ab46e6625870805b2c42
Jason Shepherd <jmanshepherd19@gmail.com>
2020-11-02 13:20:58 -0600

    refactored interpretation, still need to do sb1-4 properly

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -1,43 +1,47 @@
 #include "Interpreter.h"
 
+//TODO seperate out SB1-4 interpretation :)
 void Interpreter::update(char byte) {
-    if(byte == '+' || byte == '-') { 
-        m_index = 0;
-        m_positive = byte == '+';
-        display();
-    } else if(m_index < 5)
-        m_data[m_index-1] = byte;
-    else if(m_index == 6)
-        m_point = (int)byte - 48;
-    else if(m_index == 10) {
-        switch((int)byte) {
-            case -128:
-                m_unit = 'V';
-                break;
-            case 64:
-                m_unit = 'A';
-                break;
-            case 32:
-                m_unit = "Ohms";
-                break;
-            case 16:
-                m_unit = "hFe";
-                break;
-            case 8:
-                m_unit = "Hz";
-                break;
-            case 4:
-                m_unit = "F";
-                break;
-            case 2:
-                m_unit = "C";
-                break;
-            case 1:
-                m_unit = "F";
-                break;
-        }
+    m_byteString[m_index] = byte;
+
+    m_positive = m_byteString[0] == '+';
+    for(int i = 0; i < 4; i++)
+        m_data[i] = m_byteString[i+1];
+    m_point = (int)m_byteString[6] - 48;
+
+    switch((int)m_byteString[10]) {
+        case -128:
+            m_unit = 'V';
+            break;
+        case 64:
+            m_unit = 'A';
+            break;
+        case 32:
+            m_unit = "Ohms";
+            break;
+        case 16:
+            m_unit = "hFe";
+            break;
+        case 8:
+            m_unit = "Hz";
+            break;
+        case 4:
+            m_unit = "F";
+            break;
+        case 2:
+            m_unit = "C";
+            break;
+        case 1:
+            m_unit = "F";
+            break;
     }
-    m_index++;
+    
+    m_index++; 
+
+    if((int)byte == 10) {
+        m_index = 0;
+        display();
+    }
 }
 
 void Interpreter::display() {
--- src/Interpreter.h
+++ src/Interpreter.h
@@ -6,10 +6,13 @@
 class Interpreter {
     private:
         int m_index = 0;
+        char m_byteString[14];
+
         bool m_positive;
         char m_data[4];
-        int m_point;
+        int m_point = 0;
         std::string m_unit = "";
+        std::string m_mode = "";
     public:
         void update(char byte);
         void display();
